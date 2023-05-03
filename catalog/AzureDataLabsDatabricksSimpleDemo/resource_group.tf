41afaea0163886d4ffb5ecaf8d5a97bc37cb8f7a
jorgedocampo <jodocamp@microsoft.com>
2023-05-03 19:53:29 +0200

    dns rg

--- catalog/AzureDataLabsDatabricksSimpleDemo/resource_group.tf
+++ catalog/AzureDataLabsDatabricksSimpleDemo/resource_group.tf
@@ -9,13 +9,13 @@
 #  tags = local.tags
 #}
 #
-module "resource_group_global_dns" {
-  source = "git::https://github.com/Azure/azure-data-labs-modules.git//terraform/resource-group?ref=v1.5.0&depth=1"
-
-  basename = "${local.basename}-global-dns"
-  location = var.location
-
-  count = var.enable_private_endpoints ? 1 : 0
-
-  tags = local.tags
-}
+#module "resource_group_global_dns" {
+#  source = "git::https://github.com/Azure/azure-data-labs-modules.git//terraform/resource-group?ref=v1.5.0&depth=1"
+#
+#  basename = "${local.basename}-global-dns"
+#  location = var.location
+#
+#  count = var.enable_private_endpoints ? 1 : 0
+#
+#  tags = local.tags
+#}
