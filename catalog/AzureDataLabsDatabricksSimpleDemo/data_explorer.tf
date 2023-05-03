9692860a0010813f9b229178a2e763d359e60128
jorgedocampo <jodocamp@microsoft.com>
2023-05-03 19:41:52 +0200

    refactor rg modules

--- catalog/AzureDataLabsDatabricksSimpleDemo/data_explorer.tf
+++ catalog/AzureDataLabsDatabricksSimpleDemo/data_explorer.tf
@@ -4,8 +4,8 @@ module "kusto_cluster" {
   source                        = "git::https://github.com/Azure/azure-data-labs-modules.git//terraform/data-explorer/kusto-cluster?ref=v1.5.0&depth=1"
   #source                        = "../../../../azure-data-labs-modules/terraform/data-explorer/kusto-cluster/"
   basename                      = local.safe_basename
-  resource_group_name           = module.resource_group.name
-  location                      = module.resource_group.location
+  resource_group_name           = var.resource_group_name
+  location                      = var.location
   is_private_endpoint           = var.enable_private_endpoints
   subnet_id                     = var.enable_private_endpoints ? module.subnet_default[0].id : null
   private_dns_zone_ids          = var.enable_private_endpoints ? [module.private_dns_zones[0].list["privatelink.westeurope.kusto.windows.net"].id] : null
@@ -17,8 +17,8 @@ module "kusto_cluster" {
 module "kusto_database" {
   source              = "git::https://github.com/Azure/azure-data-labs-modules.git//terraform/data-explorer/kusto-database?ref=v1.5.0&depth=1"
   basename            = local.safe_basename
-  resource_group_name = module.resource_group.name
-  location            = module.resource_group.location
+  resource_group_name = var.resource_group_name
+  location            = var.location
   cluster_name        = module.kusto_cluster.name
   module_enabled      = var.enable_data_explorer
   tags                = local.tags
