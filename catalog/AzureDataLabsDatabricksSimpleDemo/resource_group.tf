2110361531c4f8fe408e780ad515f83dcaa74496
Jorge Docampo <jorge.docampo@outlook.com>
2023-05-04 18:10:25 +0200

    Update resource_group.tf

--- catalog/AzureDataLabsDatabricksSimpleDemo/resource_group.tf
+++ catalog/AzureDataLabsDatabricksSimpleDemo/resource_group.tf
@@ -4,7 +4,7 @@ module "resource_group" {
   source = "git::https://github.com/Azure/azure-data-labs-modules.git//terraform/resource-group?ref=v1.5.0&depth=1"
 
   basename = local.basename
-  location = module.resource_group.location
+  location = var.location
 
   tags = local.tags
 }
@@ -13,9 +13,24 @@ module "resource_group_global_dns" {
   source = "git::https://github.com/Azure/azure-data-labs-modules.git//terraform/resource-group?ref=v1.5.0&depth=1"
 
   basename = "${local.basename}-global-dns"
-  location = module.resource_group.location
+  location = var.location
 
   count = var.enable_private_endpoints ? 1 : 0
 
   tags = local.tags
+}
+  
+# Add the owner role assignment to the resource group
+resource "azurerm_role_assignment" "resource_group_owner" {
+  scope                = module.resource_group.id
+  role_definition_name = "Owner"
+  principal_id         = var.principal_id
+}
+
+resource "azurerm_role_assignment" "resource_group_global_dns_owner" {
+  scope                = module.resource_group_global_dns[0].id
+  role_definition_name = "Owner"
+  principal_id         = var.principal_id
+
+  count = var.enable_private_endpoints ? 1 : 0
 }
